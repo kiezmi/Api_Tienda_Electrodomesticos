01088220a9a2e8230d9f9096aa689dc987bcd2ab
Enrique de Miguel <Kiezmi@gmail.com>
2018-10-25 13:49:24 +0200

    Creacion del metodo Calcular precios

--- src/main/java/com/kiezmi/controllers/MasterCtr.java
+++ src/main/java/com/kiezmi/controllers/MasterCtr.java
@@ -1,11 +1,11 @@
 package com.kiezmi.controllers;
 
-
 import com.kiezmi.models.Electrodomestico;
 import com.kiezmi.models.Lavadora;
 import com.kiezmi.models.Televisor;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MasterCtr {
 
@@ -32,23 +32,32 @@ public class MasterCtr {
         return Electrodomestico.Colores.valueOf(color);
     }
 
-    public static Electrodomestico[] crearArrayElectrodomesticos(){
+    public static Electrodomestico[] crearArrayElectrodomesticos() {
 
-        Electrodomestico[] electrodomesticos= new Electrodomestico[10];
+        Electrodomestico[] electrodomesticos = new Electrodomestico[10];
 
         electrodomesticos[0] = new Electrodomestico();
-        electrodomesticos[1] = new Electrodomestico(100, 20, "rojo",'A');
-        electrodomesticos[2] = new Electrodomestico(100, 20, "rojo",'A');
-        electrodomesticos[3] = new Electrodomestico(100, 20, "rojo",'A');
-        electrodomesticos[4] = new Electrodomestico(100, 20, "rojo",'A');
-        electrodomesticos[5] = new Lavadora(100, 20, "rojo",'A',45);
-        electrodomesticos[6] = new Lavadora(100, 20, "rojo",'A',35);
-        electrodomesticos[7] = new Electrodomestico(100, 20, "rojo",'A');
-        electrodomesticos[8] = new Televisor(50, 5, "metal",'D',30,false);
-        electrodomesticos[9] = new Televisor(50, 5, "metal",'D',30,false);
+        electrodomesticos[1] = new Electrodomestico(100, 20, "ROJO", 'A');
+        electrodomesticos[2] = new Electrodomestico(200, 30, "VERDE", 'B');
+        electrodomesticos[3] = new Lavadora(100, 20, "ROJO", 'F', 35);
+        electrodomesticos[4] = new Electrodomestico(400, 50, "METAL", 'D');
+        electrodomesticos[5] = new Lavadora(100, 20, "AZUL", 'E', 45);
+        electrodomesticos[6] = new Electrodomestico(300, 40, "BLANCO", 'C');
+        electrodomesticos[7] = new Televisor(50, 5, "METAL", 'A', 30, false);
+        electrodomesticos[8] = new Electrodomestico(100, 20, "ROJO", 'G');
+        electrodomesticos[9] = new Televisor(50, 5, "VERDE", 'C', 30, true);
 
-        return  electrodomesticos;
+        return electrodomesticos;
     }
+    public static Electrodomestico calcularPrecios(Electrodomestico electrodomesticos){
+    double precios;
 
+        for(int i = 0;i<10;i++){
+
+
+
+        }
+        return precios;
+    }
 
 }
